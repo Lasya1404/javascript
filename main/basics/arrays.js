afd0b440945dd83a9a023652c4b8f36b2c73ee51
lasya <126437621+Lasya1404@users.noreply.github.com>
2024-01-23 05:19:55 +0000

    additional array

--- main/basics/arrays.js
+++ main/basics/arrays.js
@@ -1,10 +1,23 @@
         let names = ["lasya" ," lavanya", "bhumika" ,"pravallika", "swetha"];
          let names2 = ["karthik" , "manesh"];
+
         //  names.push(names2);
         //  console.log(names);
          //for push--> here we get names2 array as an element into the names array instead of having the elements from names2 array individually.(array inside an array).
-         let allNames = names.concat(names2)
-         console.log(allNames)
-         //concat gives a new array without having array inside an array
+
+
+//         //  let allNames = names.concat(names2)
+//         //  console.log(allNames)
+//          //concat gives a new array without having array inside an array
          
-//we have an easier option than concat which is spread operator....
+// //we have an easier option than concat which is spread operator....
+//         let name01 = [...names , ...names2];
+//         console.log(name01);
+
+
+// //converting anything to an array
+//         // var color ="pink";
+//         // console.log(Array.isArray(color));
+//         // var color_array = Array.from(color);
+//         // console.log(color_array);
+//         // console.log(Array.of(color));
